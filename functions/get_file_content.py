b4a8be098c6e81d2321d0f1d0b8f17dd0cc55a4a
Bence Fekete <fekete965@gmail.com>
2025-09-08 16:00:37 +0100

    Update get_file_content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -4,17 +4,17 @@ from config import MAX_CONTENT_CHUNK_SIZE
 
 def get_file_content_unsafe(working_directory, file_path):
   working_dir_abs_path = path.abspath(working_directory)
-  file_path_abs_path = path.abspath(path.join(working_directory, file_path))
+  target_file_path_abs_path = path.abspath(path.join(working_directory, file_path))
   
-  if not file_path_abs_path.startswith(working_dir_abs_path):
+  if not target_file_path_abs_path.startswith(working_dir_abs_path):
     return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
   
-  if not path.isfile(file_path_abs_path):
+  if not path.isfile(target_file_path_abs_path):
     return f'Error: File not found or is not a regular file: "{file_path}"'
   
   result = None
   
-  with open(file_path_abs_path, 'r') as file:
+  with open(target_file_path_abs_path, 'r') as file:
     result = file.read(MAX_CONTENT_CHUNK_SIZE)
 
   if len(result) ==  MAX_CONTENT_CHUNK_SIZE:
