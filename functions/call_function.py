a88e7954cc0523e91f4b13826a554421f5db9f16
Bence Fekete <fekete965@gmail.com>
2025-09-13 13:26:21 +0100

    Fix get_files_info function call, set default arg for directory

--- functions/call_function.py
+++ functions/call_function.py
@@ -21,19 +21,29 @@ def call_function(function_call_part: types.FunctionCall, verbose: bool = False)
     match function_name:
         case 'get_file_content':
             result = get_file_content(
-                working_directory=WORKING_DIR, file_path=function_args.get('file_path'))
+                working_directory=WORKING_DIR,
+                file_path=function_args.get('file_path'),
+            )
             return function_result_to_content(function_name=function_name, function_result=result)
         case 'get_files_info':
             result = get_files_info(
-                working_directory=WORKING_DIR, directory=function_args.get('directory'))
+                working_directory=WORKING_DIR,
+                directory=function_args.get('directory', '.'),
+            )
             return function_result_to_content(function_name=function_name, function_result=result)
         case 'run_python_file':
-            result = run_python_file(working_directory=WORKING_DIR, file_path=function_args.get(
-                'file_path'), args=function_args.get('args', []))
+            result = run_python_file(
+                working_directory=WORKING_DIR,
+                file_path=function_args.get('file_path'),
+                args=function_args.get('args', []),
+            )
             return function_result_to_content(function_name=function_name, function_result=result)
         case 'write_file':
-            result = write_file(working_directory=WORKING_DIR, file_path=function_args.get(
-                'file_path'), content=function_args.get('content'))
+            result = write_file(
+                working_directory=WORKING_DIR,
+                file_path=function_args.get('file_path'),
+                content=function_args.get('content'),
+            )
             return function_result_to_content(function_name=function_name, function_result=result)
         case function_name:
             return function_call_error(function_name=function_name)
